032fbfa89a4658dcbbde3eb8d2a78501006cf0e5
Ian Gutwinski <iangutwinski@gmail.com>
2019-01-22 01:32:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -70,7 +70,7 @@ end
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
     board[combo[0]] == board[combo[1]] &&
-    board[combo[2]] == board[combo[3]] &&
+    board[combo[1]] == board[combo[2]] &&
     position_taken(board,combo[0])
   end
 end
